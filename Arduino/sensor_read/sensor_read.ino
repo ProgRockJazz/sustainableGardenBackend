58b27818e5416d469cec52ba3d1fb7b68ad97aad
Paxton Schweigert <paxmanrocks@gmail.com>
2021-06-22 20:23:06 -0700

    Refactor arduino code + add rain sensor

--- Arduino/sensor_read/sensor_read.ino
+++ Arduino/sensor_read/sensor_read.ino
@@ -1,50 +1,57 @@
 #include <ArduinoJson.h>
 #include <DHT.h>
 
+#define NPINS 12 // pin count
+
 void setup() {
   Serial.begin(115200);
   while (!Serial) continue;
-
 }
+
+void readSensor(String sensorName, int sensorPin) {
+  if (sensorPin >= 2 && sensorPin <= NPINS && sensorName != "") {
+    StaticJsonDocument<200> out;
+
+    if (sensorName.equals("DHT11")) {
+      DHT dht(sensorPin, DHT11);
+
+      dht.begin();
+      delay(500);
+
+      float h = dht.readHumidity();
+      float t = dht.readTemperature();
+
+      if (isnan(h) || isnan(t)) {
+        Serial.print("Sensor reading failed!");
+        return;
+      }
+
+      out["Temperature"] = t;
+      out["Humidity"] = h;
+    }
+    else if (sensorName.equals("Rain")) {
+      int reading = digitalRead(sensorPin);
+      out["Rain"] = reading;
+    }
+    
+    serializeJsonPretty(out, Serial);
+  }
+
+  else {
+    Serial.println("Invalid input!");
+  }
+}
+
 void loop() {
   if (Serial.available()) {
-    StaticJsonDocument<300> doc;
+    StaticJsonDocument<200> doc;
     DeserializationError err = deserializeJson(doc, Serial);
 
     if (err == DeserializationError::Ok) {
-
-      
       long pinin = doc["pin"];
       String sensor = doc["sensor"];
 
-
-      
-      if (pinin >= 2 && pinin <= 12 && sensor != "") {
-        if (sensor.equals("DHT11")) {
-          DHT dht(pinin, DHT11);
-
-          dht.begin();
-
-          delay(2000);
-
-         float h = dht.readHumidity();
-        float t = dht.readTemperature();
-
-        if (isnan(h) || isnan(t)) {
-          Serial.print("Sensor reading failed!");
-          return;
-        }
-
-        Serial.print("  Temp: ");
-        Serial.println(t);
-        Serial.print("  Humidity: ");
-        Serial.println(h);
-        }
-       
-      }
-      else {
-        Serial.println("Invalid input!");
-      }
+      readSensor(sensor, pinin);
     }
   }
 }
