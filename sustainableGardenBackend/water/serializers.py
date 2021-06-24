2aad70e8a7503ac12c53f5ee46c105cb3e2cbbdc
Paxton Schweigert <paxmanrocks@gmail.com>
2021-06-23 19:24:07 -0700

    Typo

--- sustainableGardenBackend/water/serializers.py
+++ sustainableGardenBackend/water/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from .models import Valve
 
 
-class ValveSerializer(serializers.modelserializer):
+class ValveSerializer(serializers.ModelSerializer):
     class Meta:
         model = Valve
         fields = ['id', 'created', 'valve_name', 'pin', 'usb_port']
