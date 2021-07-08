8c6c41b14d1cca74b19f1334b0d252ff9ede8edd
Paxton Schweigert <paxmanrocks@gmail.com>
2021-07-08 11:31:42 -0700

    Setup the crontab job (set for every 30 minutes right now) and the respective cron.py which has the function read_all_sensors which makes a request to our SensorReadAll view and then saves these readings as an instance of the SensorReading model for each reading taken.

---
+++ sustainableGardenBackend/sensors/cron.py
@@ -0,0 +1,17 @@
+import requests
+import json
+from .models import Sensor, SensorReading
+from sustainableGardenBackend.settings import ALLOWED_HOSTS
+
+
+def read_all_sensors():
+    url = "http://" + ALLOWED_HOSTS[0] + ":8000/sensors/all/read"
+    data = requests.get(url)
+
+    readings = json.loads(data.content.decode())
+
+    for reading in readings:
+        sensor_pk = reading.pop('sensor_pk')
+        sensor = Sensor.objects.get(pk=sensor_pk)
+        reading = SensorReading(sensor=sensor, reading=reading)
+        reading.save()
