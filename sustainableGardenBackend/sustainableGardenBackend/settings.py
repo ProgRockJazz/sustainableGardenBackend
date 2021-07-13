b93978d8cb4893fdd12c41b1ab35e6dc041712de
Paxton Schweigert <paxmanrocks@gmail.com>
2021-07-12 18:46:13 -0700

    add Dockerfile and docker-compose.yml for docker image generation.

--- sustainableGardenBackend/sustainableGardenBackend/settings.py
+++ sustainableGardenBackend/sustainableGardenBackend/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'NAME': 'sustainablegarden',
         'USER': 'sustainablegardenuser',
         'PASSWORD': 'password',
-        'HOST': '127.0.0.1',
+        'HOST': 'db',
         'PORT': '5432',
     }
 }
