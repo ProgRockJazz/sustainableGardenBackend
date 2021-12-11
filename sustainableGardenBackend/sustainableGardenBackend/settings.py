19db50fa51bf65dc8cd864fa9022308893600e80
JosephCarey-cpp <joseph.carey@my.gavilan.edu>
2021-12-11 11:30:54 -0800

    Changed ALLOWED_ HOSTS to include IP address for data to be obtained by app

--- sustainableGardenBackend/sustainableGardenBackend/settings.py
+++ sustainableGardenBackend/sustainableGardenBackend/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'CHANGE_ME'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', "10.0.2.2"]
 
 # Application definition
 
