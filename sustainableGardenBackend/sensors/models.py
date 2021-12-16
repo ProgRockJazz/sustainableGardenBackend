6accfb6f81756f65aecd61753284596ab4c29d2c
JosephCarey-cpp <joseph.carey@my.gavilan.edu>
2021-12-15 16:08:52 -0800

    Testing changes

--- sustainableGardenBackend/sensors/models.py
+++ sustainableGardenBackend/sensors/models.py
@@ -4,7 +4,7 @@ SENSOR_CHOICES = [('DHT11', 'DHT11 - Humidity/Temperature'),
                   ('Rain', 'Rain Sensor'),
                   ]
 
-USB_CHOICES = [('/dev/ttyTEST2', 'USB Port 0'),
+USB_CHOICES = [('/dev/ttyACM0', 'USB Port 0'),
                ('/dev/ttyUSB1', 'USB Port 1'),
                ('/dev/ttyUSB2', 'USB Port 2'),
                ('/dev/ttyUSB3', 'USB Port 3'),
@@ -15,7 +15,7 @@ class Sensor(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     sensor_name = models.CharField(max_length=50, blank=True, default='')
     sensor_type = models.CharField(choices=SENSOR_CHOICES, default='DHT11', max_length=50)
-    usb_port = models.CharField(choices=USB_CHOICES, default='/dev/ttyUSB0', max_length=50)
+    usb_port = models.CharField(choices=USB_CHOICES, default='/dev/ttyACM0', max_length=50)
     pin = models.IntegerField()
     in_use = models.BooleanField(default=True)
 
